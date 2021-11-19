191d9f6035eacb622cb842d2292b3b6e747b978d
bamjam101 <herejamil123@gmail.com>
2021-11-19 12:04:49 +0530

    1. Contact Us layout by Gaurav, 2. Notice Page layout, 3. Register Page layout by Jamil, lastly 4. Modification to Home Page and other sync maintainance

---
+++ Register/script.js
@@ -0,0 +1,18 @@
+// function emailCheck() {
+//     var mail = document.getElementById("email").value;
+
+//     var regx = /^[a-zA-Z0-9_!#$%&'+/=?`{|}~^-]+(?:\\.[a-zA-Z0-9_!#$%&'+/=?`{|}~^-]+)@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)$/
+
+//     if(regx.text(mail)) {
+//         return true;
+//     } else {
+//         alert("Not a valid E-mail format, Please try again!");
+//         document.getElementById("email").focus();
+//         return false;
+//     }
+
+// }
+
+// function btn() {
+//     document.getElementById("proceed-btn").style.borderRadius = "3px solid black"
+// }
