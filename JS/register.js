51b3bf9fe3057ee587f4ce7468e5d7249f32b74c
bamjam101 <herejamil123@gmail.com>
2021-12-01 17:18:09 +0530

    Added Submit page for form-2, and organized the repository.

---
+++ JS/register.js
@@ -0,0 +1,12 @@
+
+// function mainFunction(event) {
+//     // const firstName = document.querySelector(".fname");
+//     // const lastName = document.querySelector(".lname");
+//     // const email = document.querySelector(".email");
+//     // const status = document.querySelectorAll(".status");
+//     event.preventDefault();
+//     let formOne = document.getElementsByClassName("form");
+//     let formTwo = document.getElementsByClassName("formTwo");
+//     formOne.style.display = "none";
+//     formTwo.style.display = "block";
+// }
