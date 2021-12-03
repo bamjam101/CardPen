ab55c7f43199e83cfd2054c4beb3da2af93932b6
Vagaur <gauravredkar2001@gmail.com>
2021-12-03 19:51:35 +0530

    Registration page made responsive and other minor fixes

--- JS/register.js
+++ JS/register.js
@@ -1,14 +1,37 @@
 
-function mainFunction() {
-    const firstName = document.querySelector(".fname");
-    const lastName = document.querySelector(".lname");
-    const email = document.querySelector(".email");
-    const status = document.querySelectorAll(".status");
+// function mainFunction() {
+//     const firstName = document.querySelector(".fname");
+//     const lastName = document.querySelector(".lname");
+//     const email = document.querySelector(".email");
+//     const status = document.querySelectorAll(".status");
     
-    let formOne = document.querySelector(".form");
-    let formTwo = document.querySelector(".formTwo");
-    formOne.style.transform = "translateX(-28vh)";
-    formOne.style.opacity = "0";
-    formTwo.style.opacity = "1";
-    formTwo.style.transform = "translateX(-28vh)";
+//     let formOne = document.querySelector(".form");
+//     let formTwo = document.querySelector(".formTwo");
+//     formOne.style.transform = "translateX(-10vh)";
+//     formOne.style.zIndex = "2";
+//     formOne.style.opacity = "0";
+//     formTwo.style.transform = "translateX(0vh)";
+//     formTwo.style.zIndex = "1000";
+//     formTwo.style.opacity = "1";
+// }
+
+function mainFunction() {
+    const firstName = document.querySelector("#fname");
+    const lastName = document.querySelector("#lname");
+    const email = document.querySelector("#email");
+
+    if (firstName.value == "" || lastName.value == "" || email.value == "") {
+        alert("Kindly Fill All The Slots!");
+        return false;
+    } else {
+        let formOne = document.querySelector(".form");
+        let formTwo = document.querySelector(".formTwo");
+        formOne.style.transform = "translateX(-10vh)";
+        formOne.style.zIndex = "2";
+        formOne.style.opacity = "0";
+        formTwo.style.transform = "translateX(0vh)";
+        formTwo.style.zIndex = "1000";
+        formTwo.style.opacity = "1";
+        return true;
+    }
 }
