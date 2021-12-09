51c9f16ff4fbede5fb8984e45d9a57c6c6870046
bamjam101 <herejamil123@gmail.com>
2021-12-10 03:52:55 +0530

    Added more details to Contact Page, and tweaked all the pages for better experience.

---
+++ JS/notice.js
@@ -0,0 +1,16 @@
+// const span = document.querySelector('.span');
+// span.classList.remove('span-transition');
+
+// // Create the observer, same as before:
+// const observer = new IntersectionObserver(entries => {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting) {
+//       span.classList.add('span-transition');
+//       return;
+//     }
+
+//     span.classList.remove('span-transition');
+//   });
+// });
+
+// observer.observe(document.querySelector('.span-wrapper'));
